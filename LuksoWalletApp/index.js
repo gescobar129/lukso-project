c280b869bd68aac0d5d707bf53bc25c01181c989
Gaida Escobar <gescobar129@gmail.com>
2022-08-02 23:22:00 -0400

    Laid out CreateWallet/ImportWallet/Dashboard UI and set up React Navigation

--- LuksoWalletApp/index.js
+++ LuksoWalletApp/index.js
@@ -3,7 +3,99 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+// import App from './App';
 import {name as appName} from './app.json';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import CreateWallet from './CreateWallet';
+import ImportWallet from './ImportWallet';
+import Dashboard from './Dashboard';
+import Collectibles from './Collectibles';
+import RecentActivity from './RecentActivity';
+import FAIcon from 'react-native-vector-icons/FontAwesome';
+import MaterialCommunityIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 
-AppRegistry.registerComponent(appName, () => App);
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const isSignedIn = false
+
+export default function Project() {
+  return (
+    <NavigationContainer>
+      {isSignedIn ? (
+        <Tab.Navigator 
+          initialRouteName="Dashboard"
+          screenOptions={{
+            tabBarStyle: {
+              borderTopColor: "#191919",
+              backgroundColor: "#191919"
+            },
+            tabBarShowLabel: false,
+            tabBarInactiveTintColor: "grey",
+            tabBarActiveTintColor: '#FFFFFF',
+
+          }}
+        >
+          <Tab.Screen 
+            name="Dashboard" 
+            component={Dashboard} 
+            options={{ 
+              headerShown: false,
+              tabBarIcon: ({color}) => (
+                <FAIcon name="dollar" color={color} size={25} />
+              ),
+            }} 
+          />
+          <Tab.Screen
+            name="Collectibles" 
+            component={Collectibles} 
+            options={{
+              title: "Collectibles",
+              tabBarIcon: ({color}) => (
+                <MaterialCommunityIcon name="view-grid" color={color} size={30} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="RecentActivity" 
+            component={RecentActivity} 
+            options={{ 
+              title: "Recent Activity",
+              tabBarIcon: ({color}) => (
+                <FAIcon name="bolt" color={color} size={25} />
+              ),
+            }} 
+          />
+        </Tab.Navigator>
+      ) : (
+        <Stack.Navigator initialRouteName="ImportWallet">
+          <Stack.Screen 
+            name="CreateWallet" 
+            component={CreateWallet} 
+            options={{ 
+              title: "Secret Recovery Phrase",
+              headerStyle: {
+                backgroundColor: "#1b1c1c"
+              }
+            }}/>
+          <Stack.Screen 
+            name="ImportWallet" 
+            component={ImportWallet} 
+            options={{
+              headerStyle: {
+                backgroundColor: "#1b1c1c",
+              },
+            }} 
+          />
+        </Stack.Navigator>
+      )}
+    </NavigationContainer>
+    );
+  };
+  
+  AppRegistry.registerComponent(appName, () => Project) 
+
